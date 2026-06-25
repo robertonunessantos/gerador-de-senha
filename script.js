36943ee6207aeaae0390cff21e33e892df4e9f6c
robertonunessantos <140280864+robertonunessantos@users.noreply.github.com>
2026-06-25 20:44:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function gerarSenha() {
     const numeros = "0123456789";
     const simbolos = "!@#$%&*";
 
-    const quantidade = document.getElementById("quantidade").value;
+    const quantidade = Number(document.getElementById("quantidade").value);
 
     const usarMaiusculas = document.getElementById("maiusculas").checked;
     const usarMinusculas = document.getElementById("minusculas").checked;
@@ -38,6 +38,7 @@ function gerarSenha() {
     if (caracteres === "") {
         campoSenha.value = "";
         mensagem.textContent = "Selecione pelo menos um tipo de caractere.";
+        atualizarForca(0);
         return;
     }
 
@@ -47,4 +48,90 @@ function gerarSenha() {
     }
 
     campoSenha.value = senha;
+
+    verificarForcaSenha(
+        quantidade,
+        usarMaiusculas,
+        usarMinusculas,
+        usarNumeros,
+        usarSimbolos
+    );
+}
+
+function verificarForcaSenha(quantidade, usarMaiusculas, usarMinusculas, usarNumeros, usarSimbolos) {
+    let pontos = 0;
+
+    if (quantidade >= 8) {
+        pontos++;
+    }
+
+    if (quantidade >= 12) {
+        pontos++;
+    }
+
+    if (usarMaiusculas) {
+        pontos++;
+    }
+
+    if (usarMinusculas) {
+        pontos++;
+    }
+
+    if (usarNumeros) {
+        pontos++;
+    }
+
+    if (usarSimbolos) {
+        pontos++;
+    }
+
+    if (pontos <= 2) {
+        atualizarForca(1);
+    } else if (pontos <= 4) {
+        atualizarForca(2);
+    } else if (pontos === 5) {
+        atualizarForca(3);
+    } else {
+        atualizarForca(4);
+    }
+}
+
+function atualizarForca(nivel) {
+    const barraForca = document.getElementById("barraForca");
+    const textoForca = document.getElementById("textoForca");
+
+    if (nivel === 0) {
+        barraForca.style.width = "0%";
+        barraForca.style.background = "#cbd5e1";
+        textoForca.textContent = "Aguardando senha";
+        textoForca.style.color = "#555";
+    }
+
+    if (nivel === 1) {
+        barraForca.style.width = "25%";
+        barraForca.style.background = "#dc2626";
+        textoForca.textContent = "Fraca";
+        textoForca.style.color = "#dc2626";
+    }
+
+    if (nivel === 2) {
+        barraForca.style.width = "50%";
+        barraForca.style.background = "#f59e0b";
+        textoForca.textContent = "Média";
+        textoForca.style.color = "#f59e0b";
+    }
+
+    if (nivel === 3) {
+        barraForca.style.width = "75%";
+        barraForca.style.background = "#16a34a";
+        textoForca.textContent = "Forte";
+        textoForca.style.color = "#16a34a";
+    }
+
+    if (nivel === 4) {
+        barraForca.style.width = "100%";
+        barraForca.style.background = "#15803d";
+        textoForca.textContent = "Muito forte";
+        textoForca.style.color = "#15803d";
+    }
 }
